ab32fce922f9bf0dc317afaadacd53066d4738e7
Sky141589 <shen.pengpeng@sunlord.com.cn>
2025-03-11 01:04:55 +0800

    L0

--- L0/02SolidityDemo/contracts/myERC20.sol
+++ L0/02SolidityDemo/contracts/myERC20.sol
@@ -1,7 +1,9 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.28;
-import "../node_modules/@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
 contract MyERC20 is ERC20 {
-    constructor() ERC20("MinerReward", "MRW") {}
+    constructor() ERC20("MinerReward", "MRW") {
+        _mint(msg.sender, 20000000*10**18);
+    }
 }
